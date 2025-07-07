ee797bdce9d19259fd214dc34d0e7afb9d0d6bd8
oCharybd1s <108251057+oCharybd1s@users.noreply.github.com>
2025-07-07 23:16:52 +0700

    Delete main directory

--- main/PATA/PopupAddNote.php
+++
@@ -1,6 +0,0 @@
-<div class="row">
-	<label class="control-label col-md-2 col-sm-2 col-xs-12" style="text-align:right;">Catatan</label>
-    <div class="col-md-6 col-sm-6 col-xs-12">
-      <textarea id="notepata" class="form-control col-md-7 col-xs-12" rows="10"></textarea>
-    </div>
-</div>
